9f4bf6237d865c8b21e55424013eb3b9bbc628cd
Monroe Chung <monroe.chung@gmail.com>
2024-04-15 01:00:47 -0400

    user works

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000);
