1d28b349af4b3047df729614578b025d34f474a3
Monroe Chung <monroe.chung@gmail.com>
2024-04-16 01:28:21 -0400

    working

--- App.js
+++ App.js
@@ -5,11 +5,34 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
 
 mongoose.connect("mongodb+srv://chungmo:kFeOCYVEMyDV3HaV@cluster0.6ikctr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {dbName: 'kanbas'});
 
 const app = express();
-app.use(cors({origin: true, credentials: true}));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+
+app.use(session(sessionOptions));
+ 
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
